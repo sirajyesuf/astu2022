2a5a2cb0bc9b7fb66e913e6e41b27fe84ec40d30
sirajyesuf <sirajyesuf762@gmail.com>
2022-06-22 12:09:44 +0300

    =students  endpoints done

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -6,13 +6,25 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Resources\StudentResource;
 use App\Models\Day;
+use App\Models\Department;
+use App\Models\School;
 use App\Models\Student;
 
 class StudentController extends Controller
 {
     public function index(Request $request)
     {
+        $school = School::where('short_name', $request->query('school'))->first();
+        $dept = Department::where('short_name', $request->query('dept'))->first();
 
+        if ($school) {
+
+            return StudentResource::collection($school->students()->paginate(2));
+        }
+
+        if ($dept) {
+            return StudentResource::collection($dept->students()->paginate(2));
+        }
 
         return StudentResource::collection(Student::paginate(2));
     }
