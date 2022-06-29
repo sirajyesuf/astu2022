1ec7ab9099d200e203c91d2b8ae5c4b5b804b6ba
sirajyesuf <sirajyesuf762@gmail.com>
2022-06-29 11:37:48 +0300

    =remove default user_id from userstable

--- database/migrations/2022_06_22_030517_add_role_id_on_users_table.php
+++ database/migrations/2022_06_22_030517_add_role_id_on_users_table.php
@@ -15,8 +15,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->unsignedBigInteger('role_id')
-                ->default(Role::where('short_name', 'admin')->first()->id);
+            $table->unsignedBigInteger('role_id');
             $table->foreign('role_id')->references('id')->on('roles');
         });
     }
