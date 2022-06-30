bda6559f9c2730cee9c4b18cbe074aab37480d41
sirajyesuf <sirajyesuf762@gmail.com>
2022-06-30 22:31:36 +0300

    =update event icon

--- app/Filament/Resources/EventResource.php
+++ app/Filament/Resources/EventResource.php
@@ -17,7 +17,7 @@ class EventResource extends Resource
 {
     protected static ?string $model = Event::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-calendar';
+    protected static ?string $navigationIcon = 'heroicon-s-calendar';
 
     public static function form(Form $form): Form
     {
