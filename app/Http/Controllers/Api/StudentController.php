c90e88c131d66a73a363aa427b64c7e0dfc4cad0
sirajyesuf <sirajyesuf762@gmail.com>
2022-07-09 22:59:26 +0300

    =student search by full name

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -8,6 +8,7 @@ use App\Http\Resources\StudentResource;
 use App\Models\Department;
 use App\Models\School;
 use App\Models\Student;
+use Illuminate\Support\Str;
 
 class StudentController extends Controller
 {
@@ -15,7 +16,8 @@ class StudentController extends Controller
     {
         $school = School::where('short_name', $request->query('school'))->first();
         $dept = Department::where('short_name', $request->query('dept'))->first();
-        $searchTerm = $request->query('search');
+        $searchTerm = Str::of($request->query('search'))->explode(' ');
+
 
         $per_page = $request->query('per_page', 14);
 
@@ -28,9 +30,17 @@ class StudentController extends Controller
             return StudentResource::collection($dept->students()->orderBy('first_name')->paginate($per_page));
         }
 
-        if ($searchTerm) {
 
-            return StudentResource::collection(Student::where('first_name', 'LIKE', "%{$searchTerm}%")->orderBy('first_name')->paginate($per_page));
+        if ($searchTerm->count() == 1) {
+            return StudentResource::collection(Student::where('first_name', 'LIKE', "%{$searchTerm[0]}%")->orderBy('first_name')->paginate($per_page));
+        }
+        if ($searchTerm->count() == 2) {
+            $fn = $searchTerm[0];
+            $ln = $searchTerm[1];
+            $students = Student::where('first_name', 'LIKE', "%{$fn}%")
+                ->where('last_name', 'LIKE', "%{$ln}%")
+                ->orderBy('first_name')->paginate($per_page);
+            return StudentResource::collection($students);
         }
 
         return StudentResource::collection(Student::orderBy('first_name')->paginate($per_page));
